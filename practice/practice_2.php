f3b83997ec7cf253045fb6bdae3aceb4685d5bd5
virag <viraggls123@gmail.com>
2024-01-18 18:26:13 +0530

    change

--- practice/practice_2.php
+++ practice/practice_2.php
@@ -1,11 +1,124 @@
 <?php 
-    $int=12;
-    var_dump($int);
+//data types
+    /*$int=12;
+    var_dump($int)."<br>";
 
     $float=12.3;
-    var_dump($float);
+    var_dump($float)."<br>";
 
     $string="hello";
-    var_dump($string);
+    var_dump($string)."<br>";
+
+    $boolean=true;
+    var_dump($boolean)."<br>";
+
+    $array=array(1,2,3,"hi");
+    var_dump($array)."<br>";
+    var_dump($array[2])."<br>";
+    $array[1]=12.2;
+    var_dump($array)."<br>";
+    array_push($array,true);
+    var_dump($array)."<br>";
+    $car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+    var_dump($car)."<br>";
+    echo $car["model"]."<br>";
+    $car["year"] = 2024;
+    var_dump($car)."<br>";
+    unset($car["year"]);
+    $cars = array (
+        array("Volvo",22,18),
+        array("BMW",15,13),
+        array("Saab",5,2),
+        array("Land Rover",17,15)
+      );
+    echo $cars[0][2]."<br>";
+
+    $null=null;
+    var_dump($null)."<br>";
+    */
+
+    //type casting
+      /*
+    $float=12.3;
+    var_dump($float)."<br>";
+    $int=(int)$float;
+    var_dump($int)."<br>";
+    $string=(string)$int;
+    var_dump($string)."<br>";
+    $string=1;
+    $bool=(boolean)$string;
+    var_dump($bool)."<br>";
+    $array=(array)$float;
+    var_dump($array)."<br>";
+    $a=1;
+    $unset=(unset) $a;
+    var_dump($unset)."<br>";
+
+    */
+
+    //math function
+    /*
+    //basic arithmetic
+    $num=-12.6;
+    echo abs($num)."<br>";
+    echo ceil($num)."<br>";
+    echo floor($num)."<br>";
+    echo round($num)."<br>";
+
+    //power function
+    echo pow($num,2)."<br>";
+    $a=64;
+    echo sqrt($a)."<br>";
+    
+    //random num
+    echo rand(0,200)."<br>";
+
+    //num formatting
+    echo number_format("1000",5,"$","@")."<br>";
+    */
+
+    //php operators
+    
+    $a=10;
+    $b=5;
+    /*
+
+    //arithmetic
+    echo $a+$b."<br>";
+    echo $a-$b."<br>";
+    echo $a*$b."<br>";
+    echo $a/$b."<br>";
+    echo $a%$b."<br>";
+    echo $a**$b."<br>";
+    */
+
+    //assignment
+    //echo $a=$b."<br>";
+    //echo $a += $b."<br>";
+    //echo $a-=$b."<br>";
+    //echo $a*=$b."<br>";
+    //echo $a/=$b."<br>";
+    //echo $a%=$b."<br>";
+    
+    /*
+
+    //comparison
+    echo var_dump($a==$b)."<br>";
+    echo var_dump($a===$b)."<br>";
+    echo var_dump($a!=$b)."<br>";
+    echo var_dump($a!==$b)."<br>";
+    echo var_dump($a>$b)."<br>";
+    echo var_dump($a<$b)."<br>";
+    echo var_dump($a>=$b)."<br>";
+    echo var_dump($a<=$b)."<br>";
+    */
+
+    //logical
+    $a=true;
+    $b=false;
+    echo var_dump($a&&$b)."<br>";
+    echo var_dump($a||$b)."<br>";
+    echo var_dump(!$a)."<br>";
+    
 
 ?>
