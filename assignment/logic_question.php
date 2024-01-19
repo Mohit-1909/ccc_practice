c1d0579d1980589e7059edbc785c3ed5ef4380d1
Mohit Dodeja <mohitdodeja513@gmail.com>
2024-01-19 17:08:13 +0530

    Harsh Sir's 10% problem

---
+++ assignment/logic_question.php
@@ -0,0 +1,20 @@
+<?php
+
+// $a; $b=100; $per;
+
+// $a = $b + 0.1*$b;
+// echo "Value of a: ". $a . "\n";
+
+// $per = (($a-$b)/$a)*100;
+
+// echo $per;
+
+
+// in negative form
+
+// $b = 100; // Initial value of b
+// $a = 1.1 * $b; // Assuming a is 10% higher than b
+
+// $percentage_higher = (($b - $a) / $a) * 100;
+
+// echo "When a is 10% higher than b, the value of b is $percentage_higher% higher than a.";
