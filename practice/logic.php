8466301ad32281dd2ad273f4f55908bf20965f34
virag <viraggls123@gmail.com>
2024-01-22 09:11:18 +0530

    changes and add file

---
+++ practice/logic.php
@@ -0,0 +1,11 @@
+<?php
+//If $a is 10% higher than $b then $b is how much lower than $a
+$hp=10;
+$b=100;
+$a=$b+(10/100)*100;
+echo "->if \$b value is 100 so value of \$a:".$ov."<br><br>";
+
+$lp= (($a - $b)/$a)*100;
+
+echo "->\$b is around <b>".round($lp,2)."%</b> lower than \$a";
+?>
