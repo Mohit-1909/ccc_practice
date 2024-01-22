1f393fdad83875177930425c7c1929b7b288f6be
virag <viraggls123@gmail.com>
2024-01-22 09:39:28 +0530

    change

--- practice/logic.php
+++ practice/logic.php
@@ -1,9 +1,9 @@
 <?php
 //If $a is 10% higher than $b then $b is how much lower than $a
-$hp=10;
+$hp=50;
 $b=100;
-$a=$b+(10/100)*100;
-echo "->if \$b value is 100 so value of \$a:".$ov."<br><br>";
+$a=$b+($hp/100)*100;
+echo "->if \$b value is 100 so value of \$a:".$a."<br><br>";
 
 $lp= (($a - $b)/$a)*100;
 
